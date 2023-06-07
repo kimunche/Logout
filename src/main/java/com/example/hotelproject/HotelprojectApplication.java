167e09a066c858c83c458fe07d531eceff5170cb
kimunche <angela_id@naver.com>
2023-06-07 21:57:26 +0900

    초기화

---
+++ src/main/java/com/example/hotelproject/HotelprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotelproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelprojectApplication.class, args);
+    }
+
+}
